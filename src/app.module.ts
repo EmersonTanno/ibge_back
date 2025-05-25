156767d2697c9f0c42f3ac20351f70fdeb892b85
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-25 19:33:12 -0300

    chore: app.module - adding comparison.module to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EvolutionModule } from './evolution/evolution.module';
 import { TrendModule } from './trend/trend.module';
+import { ComparisonModule } from './comparison/comparison.module';
 
 @Module({
-  imports: [EvolutionModule, TrendModule],
+  imports: [EvolutionModule, TrendModule, ComparisonModule],
   controllers: [AppController],
   providers: [AppService],
 })
