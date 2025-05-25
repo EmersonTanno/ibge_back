e34f502f133c1ef54e33834001657599d2c6f2ac
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-25 17:57:20 -0300

    chore: app.module - adding TrendModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EvolutionModule } from './evolution/evolution.module';
+import { TrendModule } from './trend/trend.module';
 
 @Module({
-  imports: [EvolutionModule],
+  imports: [EvolutionModule, TrendModule],
   controllers: [AppController],
   providers: [AppService],
 })
